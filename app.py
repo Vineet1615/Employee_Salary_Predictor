264b807a9f49f29d9e8b22fd8459c2f57354a756
vineet <vineet3424@gmail.com>
2025-07-30 20:29:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -121,13 +121,11 @@ with tabs[0]:
     
     **Why Use the Employee Salary Predictor?**
 
-- Save time: Automate salary benchmarking for any candidate profile in seconds.
-- Remove guesswork: Data-driven models capture real industry factors and experience.
-- Visualize insights: Compare compensation by demographics, roles, and qualifications.
-- Empower HR and employees: Promote pay transparency and informed decision-making.
-- Secure, seamless, and fully accessible—no data upload or configuration needed.
-
-    """)
+   - Save time: Automate salary benchmarking for any candidate profile in seconds.
+   - Remove guesswork: Data-driven models capture real industry factors and experience.
+   - Visualize insights: Compare compensation by demographics, roles, and qualifications.
+   - Empower HR and employees: Promote pay transparency and informed decision-making.
+   - Secure, seamless, and fully accessible—no data upload or configuration needed.  """)
     st.info("The app uses the built-in default dataset `Salary-Data.csv`.", icon="ℹ️")
     df = predictor.load_and_preprocess_data()
     st.session_state.df = df
